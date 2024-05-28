0cbfee3734e03ec030364352dab549e3e632111c
cxz6411 <cxz6411@gmail.com>
2024-05-28 18:35:34 +0900

    add: 좋아요 및 답변 카운트 추가

--- app/src/main/java/com/example/mpproject/data/Survey.kt
+++ app/src/main/java/com/example/mpproject/data/Survey.kt
@@ -6,10 +6,12 @@ data class Survey(
     val creatorId: String = "",
     val title: String = "",
     val content: String = "",
+    val likes : Int = 0,
+    val responses : Int = 0,
     val createdDate: String = "",
     val modifiedDate: String = "",
     val status: String = "",
     val questions: Map<String, Question> = emptyMap()
 ) {
-    constructor() : this("", "", "", "", "", "", "", emptyMap())
+    constructor() : this("", "", "", "", 0,0,"", "", "", emptyMap())
 }
